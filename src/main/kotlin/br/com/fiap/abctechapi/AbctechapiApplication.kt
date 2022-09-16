44f8e781e65ed6c2eeb754e968c51e0ace902be3
Wiliam Otto <wiliam.keltke@gmail.com>
2022-09-16 18:54:11 -0300

    remove maven for gradle, add linter, fix lint error

--- src/main/kotlin/br/com/fiap/abctechapi/AbctechapiApplication.kt
+++ src/main/kotlin/br/com/fiap/abctechapi/AbctechapiApplication.kt
@@ -1,10 +1,7 @@
 package br.com.fiap.abctechapi
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 open class AbctechapiApplication
