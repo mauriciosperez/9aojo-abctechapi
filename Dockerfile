995495a9011fc877cf3198bf9bfb4c9a21017a3e
Wiliam Otto <wiliam.keltke@gmail.com>
2022-09-18 12:30:21 -0300

    add Dockerfile and fix application

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM tomcat:8.5-jdk11-corretto-al2
+
+COPY ROOT.war /usr/local/tomcat/webapps/
--- src/main/kotlin/br/com/fiap/abctechapi/AbctechapiApplication.kt
+++ src/main/kotlin/br/com/fiap/abctechapi/AbctechapiApplication.kt
@@ -1,10 +1,11 @@
 package br.com.fiap.abctechapi
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 open class AbctechapiApplication
+
 fun main(args: Array<String>) {
-    SpringApplication.run(AbctechapiApplication::class.java, *args)
+    runApplication<AbctechapiApplication>(*args)
 }
