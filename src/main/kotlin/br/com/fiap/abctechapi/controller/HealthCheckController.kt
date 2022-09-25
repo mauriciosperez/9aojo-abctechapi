5b38e3bcc6b9f9ca56b8838b271d6387d2148881
Wiliam Otto <wiliam.keltke@gmail.com>
2022-09-25 12:26:55 -0300

    update /version endpoint

--- src/main/kotlin/br/com/fiap/abctechapi/controller/HealthCheckController.kt
+++ src/main/kotlin/br/com/fiap/abctechapi/controller/HealthCheckController.kt
@@ -1,24 +1,16 @@
 package br.com.fiap.abctechapi.controller
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.io.IOException
-import java.util.Properties
 
 @RestController
 @RequestMapping("/")
-class HealthCheckController {
+class HealthCheckController @Autowired constructor(private val buildProperties: BuildProperties) {
     @GetMapping("version")
-    fun status(): ResponseEntity<String> {
-        val properties = Properties()
-        val inputStream = javaClass.classLoader.getResourceAsStream("application.yml")
-        return try {
-            properties.load(inputStream)
-            ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"))
-        } catch (e: IOException) {
-            ResponseEntity.badRequest().build()
-        }
-    }
+    fun status(): ResponseEntity<String> =
+        ResponseEntity.ok("${buildProperties.name}-${buildProperties.version}")
 }
