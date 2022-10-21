8e7d8c46672a5ef0a983ef690bbfe7f404e22a42
Wiliam Otto <wiliam.keltke@gmail.com>
2022-10-20 23:15:39 -0300

    fix lint errors

--- src/main/kotlin/br/com/fiap/abctechapi/security/jwt/AuthEntryPointJwt.kt
+++ src/main/kotlin/br/com/fiap/abctechapi/security/jwt/AuthEntryPointJwt.kt
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
-import java.util.*
+import java.util.Date
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
